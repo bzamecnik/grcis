15c85f486163b5d6bcb1c6f540e4dc7d4bca6d7c
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2010-11-22 20:57:06 +0100

    Import the grcis repository from Subversion at revision 27.

---
+++ 002warping/WarpMagniGlass.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _002warping
+{
+  public class WarpMagniGlass : DefaultWarp
+  {
+    protected bool dirty = true;
+
+    protected double factor = 1.0;
+
+    public double Factor
+    {
+      get
+      {
+        return factor;
+      }
+      set
+      {
+        factor = value;
+        dirty  = true;
+      }
+    }
+
+    protected double aspectRatioTrimmed, halfFactorOffseted, halfOfAspectRatioX, halfInverseFactorOffseted, halfOfAspectRatioY;
+
+    public override void F ( double x, double y, out double u, out double v )
+    {
+      if ( dirty )
+      {
+        double aspectRatio = iwidth / (double)iheight;
+        aspectRatioTrimmed = 0.5 * Math.Min( 1.0, aspectRatio );
+        halfFactorOffseted = 0.5 * (factor - 1.0);
+        halfOfAspectRatioX = 0.5 * aspectRatio;
+        halfInverseFactorOffseted = 0.5 * (1.0 / factor - 1.0);
+        dirty = false;
+      }
+      x /= iwidth;
+      y /= iheight;
+      double ax = (x - halfOfAspectRatioX) / aspectRatioTrimmed;
+      double ay = (y - 0.5) / aspectRatioTrimmed;
+      double rr = ax * ax + ay * ay;    // radius squared
+      double mag = aspectRatioTrimmed * Math.Exp( halfFactorOffseted * Math.Log( rr ) );
+      u = halfOfAspectRatioX + mag * ax;
+      v = 0.5 + mag * ay;
+      u *= owidth;
+      v *= oheight;
+    }
+
+    public override void FInv ( double u, double v, out double x, out double y )
+    {
+      if ( dirty )
+      {
+        double aspectRatio = iwidth / (double)iheight;
+        aspectRatioTrimmed = 0.5 * Math.Min( 1.0, aspectRatio );
+        halfFactorOffseted = 0.5 * (factor - 1.0);
+        halfOfAspectRatioX = 0.5 * aspectRatio;
+        halfOfAspectRatioY = 0.5 * 1 / aspectRatio;
+        halfInverseFactorOffseted = 0.5 * (1.0 / factor - 1.0);
+        dirty = false;
+      }
+      u /= owidth;
+      v /= oheight;
+      double ax = (u - 0.5) / aspectRatioTrimmed;
+      double ay = (v - 0.5) / aspectRatioTrimmed;
+      double rr = ax * ax + ay * ay;    // radius squared
+      double mag = rr == 0 ? 0 : aspectRatioTrimmed * Math.Exp( halfInverseFactorOffseted * Math.Log( rr ) );
+      x = 0.5 + mag * ax;
+      y = 0.5 + mag * ay;
+      x *= iwidth;
+      y *= iheight;
+    }
+  }
+}
---
+++ 008kdtree/KDTree.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+
+namespace _008kdtree
+{
+
+  public struct Segment2D
+  {
+    public float x1, y1, x2, y2;
+
+    public Segment2D ( float _x1, float _y1, float _x2, float _y2 )
+    {
+      x1 = _x1; y1 = _y1;
+      x2 = _x2; y2 = _y2;
+    }
+  }
+
+  class KDTree
+  {
+    #region protected data
+
+    protected List<Segment2D> segs;
+
+    #endregion
+
+    #region constructor
+
+    public KDTree ()
+    {
+      segs = null;
+    }
+
+    #endregion
+
+    #region stat data
+
+    protected long segmentCounter = 0L;
+
+    protected long boxCounter = 0L;
+
+    protected long heapCounter = 0L;
+
+    #endregion
+
+    #region KD-tree API
+
+    public void BuildTree ( List<Segment2D> segments )
+    {
+      // !!!{{ TODO: add the KD-tree bulding code here
+
+      segs = segments;
+
+      // !!!}}
+    }
+
+    public int RayIntersect ( float ox, float oy, float dx, float dy, int k, int[] result )
+    {
+      // !!!{{ TODO: add the intersection code here
+
+      if ( segs == null || k <= 0 ) return 0;
+
+      SortedList<double, int> res = new SortedList<double, int>();
+      for ( int i = 0; i < segs.Count; i++ )
+      {
+        Segment2D seg = segs[ i ];
+        double dist = RaySegment2D( ox, oy, dx, dy, seg.x1, seg.y1, seg.x2, seg.y2 );
+        segmentCounter++;
+        if ( dist >= 0.0 )
+          res.Add( dist, i );
+      }
+
+      int n = Math.Min( k, res.Count );
+      for ( int i = 0; i < n; i++ )
+        result[ i ] = res.ElementAt( i ).Value;
+
+      return n;
+
+      // !!!}}
+    }
+
+    public void GetStatistics ( out long segmentHits, out long boxHits, out long heapOperations )
+    {
+      // !!!{{ TODO: return requested statistics here
+
+      segmentHits = segmentCounter;
+      segmentCounter = 0L;
+
+      boxHits = boxCounter;
+      boxCounter = 0L;
+
+      heapOperations = heapCounter;
+      heapCounter = 0L;
+
+      // !!!}}
+    }
+
+    #endregion
+
+    #region support
+
+    public static double RaySegment2D ( double ox, double oy, double dx, double dy,
+                                        double ax, double ay, double bx, double by )
+    {
+      double nx  = ay - by;
+      double ny  = bx - ax;
+      double den = nx * dx + ny * dy;
+      if ( den > -2.0 * Double.Epsilon &&
+           den <  2.0 * Double.Epsilon ) return Double.NegativeInfinity;
+
+      double t   = (nx * (ax - ox) + ny * (ay - oy)) / den;
+      double resol;
+
+      if ( Math.Abs( ny ) > Math.Abs( nx ) )
+      {                                     // use X coordinate
+        resol = ox + t * dx;
+        if ( resol < Math.Min( ax, bx ) ||
+             resol > Math.Max( ax, bx ) ) return Double.NegativeInfinity;
+      }
+      else
+      {                                     // use Y coordinate
+        resol = oy + t * dy;
+        if ( resol < Math.Min( ay, by ) ||
+             resol > Math.Max( ay, by ) ) return Double.NegativeInfinity;
+      }
+
+      return t;
+    }
+
+    #endregion
+  }
+
+}
