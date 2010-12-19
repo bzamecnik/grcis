c1ece4095d6484cd786a746538fab81f5c280672
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2010-12-19 22:43:25 +0100

    Create a basic skeleton of a buffered bit stream interface.

---
+++ 011compressionbw/BufferedBitStream.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _011compressionbw
+{
+    class BufferedBitStream
+    {
+        public System.IO.Stream Stream { get; private set; }
+
+        private int buffer;
+        private int bufferLength;
+
+        public BufferedBitStream(System.IO.Stream wrappedStream)
+        {
+            Stream = wrappedStream;
+        }
+
+        public int GetBits(int bitsCount)
+        {
+            // TODO
+            return 0;
+        }
+
+        public void PutBits(int value, int bitsCount)
+        {
+            // TODO
+        }
+    }
+}
